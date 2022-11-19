a9b5f7e3e25673ff7ad943ab73628d8f51ec7ced
Frank Araque Cadenas <frank.araquecadenas@infor.com>
2022-11-19 12:27:13 +0100

    add data-cy attributtes

--- app.js
+++ app.js
@@ -3,7 +3,8 @@ const  nodemailer = require("nodemailer"),
        app        = express(),
        bodyParser = require('body-parser'),
        flash = require('connect-flash');
- 
+const port = 3000;
+
 app.use(bodyParser.urlencoded({ extended: true }));      
 app.use(express.static(__dirname + "/public"));
 app.set("view engine", "ejs");
@@ -39,9 +40,10 @@ app.get("*", (req, res)=>{
     res.redirect("/");
 });
 
-
-app.listen(process.env.PORT, process.env.IP, function(){
-    console.log("The app is up and running");
+//process.env.PORT
+//process.env.IP
+app.listen(port, function(){
+    console.log("The app is up and running in port:"+ port);
 });
 
 function enviarEmail(req, res , next){
